f7f29e250485e8cceb809343d2def51d8115f282
VladRynchenko <41292054+VladRynchenko@users.noreply.github.com>
2021-08-03 18:34:22 +0300

    small fixes

--- app/src/main/java/com/example/githubapp/MyApplication.kt
+++ app/src/main/java/com/example/githubapp/MyApplication.kt
@@ -3,7 +3,6 @@ package com.example.githubapp
 import android.app.Application
 import com.example.githubapp.di.DaggerAppComponent
 
-
 open class MyApplication : Application() {
     val appComponent by lazy {
         DaggerAppComponent.factory().create(applicationContext)
