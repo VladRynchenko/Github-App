562a1aff00c98dc7ac009f99036b5edb74737683
VladRynchenko <41292054+VladRynchenko@users.noreply.github.com>
2021-08-04 15:23:13 +0300

    Divide business logic

--- app/src/main/java/com/example/githubapp/main/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/main/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.githubapp.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.githubapp.MyApplication
 import com.example.githubapp.R
 import com.example.githubapp.viewmodels.ViewModelProvideFactory
-import com.example.githubapp.repository.LoginRepository
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -17,17 +15,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModelProvider: ViewModelProvideFactory
     lateinit var viewModel: MainViewModel
 
-    @Inject
-    lateinit var loginRepos: LoginRepository
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         (applicationContext as MyApplication).appComponent.inject(this)
         viewModel = ViewModelProvider(this, viewModelProvider).get(MainViewModel::class.java)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (loginRepos.getToken() == null) {
+        if (!viewModel.IsUserLogin()) {
             val navHostFragment = supportFragmentManager
                 .findFragmentById(R.id.myNavHostFragment) as NavHostFragment
             navHostFragment.navController.navigate(R.id.loginFragment)
