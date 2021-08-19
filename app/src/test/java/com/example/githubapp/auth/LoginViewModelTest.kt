6f204895f55bf2a74455981220a4e8979f7ff08d
VladRynchenko <41292054+VladRynchenko@users.noreply.github.com>
2021-08-19 16:14:19 +0300

    Test ViewModel

---
+++ app/src/test/java/com/example/githubapp/auth/LoginViewModelTest.kt
@@ -0,0 +1,53 @@
+package com.example.githubapp.auth
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.githubapp.RxSchedulerRule
+import com.example.githubapp.api.AccessToken
+import com.example.githubapp.getOrAwaitValue
+import com.example.githubapp.repository.LoginRepository
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.verify
+
+class LoginViewModelTest {
+
+    lateinit var repository: LoginRepository
+    private lateinit var viewModel: LoginViewModel
+    private val token = AccessToken("token", "tokenType")
+
+    @get:Rule
+    val rxSchedulerRule = RxSchedulerRule()
+
+    @Rule
+    @JvmField
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        repository = Mockito.mock(LoginRepository::class.java)
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+        viewModel = LoginViewModel(repository)
+        Mockito.`when`(repository.getAccessToken("code")).thenReturn(Observable.just(token))
+        Mockito.`when`(repository.getToken()).thenReturn(token)
+    }
+
+    @Test
+    fun getAccessToken() {
+        viewModel.getAccessToken("code")
+        verify(repository).saveToken(token)
+    }
+
+    @Test
+    fun getSavedToken() {
+        viewModel.getSavedToken()
+        verify(repository).getToken()
+        val value = viewModel.token.getOrAwaitValue()
+        assertEquals(token, value)
+    }
+}
