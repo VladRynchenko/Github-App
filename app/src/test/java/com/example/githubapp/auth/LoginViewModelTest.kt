671d79c0f9c4ea15b03bb7978a32bedf5048c48e
VladRynchenko <41292054+VladRynchenko@users.noreply.github.com>
2021-08-20 10:44:15 +0300

    Small fix

--- app/src/test/java/com/example/githubapp/auth/LoginViewModelTest.kt
+++ app/src/test/java/com/example/githubapp/auth/LoginViewModelTest.kt
@@ -3,12 +3,9 @@ package com.example.githubapp.auth
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.githubapp.RxSchedulerRule
 import com.example.githubapp.api.AccessToken
-import com.example.githubapp.getOrAwaitValue
 import com.example.githubapp.repository.LoginRepository
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.plugins.RxJavaPlugins
-import io.reactivex.rxjava3.schedulers.Schedulers
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -33,7 +30,6 @@ class LoginViewModelTest {
     @Before
     fun setUp() {
         repository = Mockito.mock(LoginRepository::class.java)
-        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
         viewModel = LoginViewModel(repository)
         Mockito.`when`(repository.getAccessToken("code")).thenReturn(Observable.just(token))
         Mockito.`when`(repository.getAccessToken("error"))
